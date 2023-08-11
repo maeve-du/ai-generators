a2b99978a0b4ee50d6a813f5002c4c1ca8ae3600
Maeve Du <maeve.du@hotmail.com>
2023-08-11 14:02:37 +0800

    fix logo text

--- components/LandingNavbar.tsx
+++ components/LandingNavbar.tsx
@@ -5,6 +5,7 @@ import { Montserrat } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Button } from './ui/button'
+import { cn } from '@/lib/utils'
 
 const font = Montserrat({
   weight: '600',
@@ -20,6 +21,7 @@ const LandingNavbar = () => {
         <div className="relative h-8 w-8 mr-4">
           <Image fill alt="Logo" src="/logo.png" />
         </div>
+        <h1 className={cn('text-2xl h-10 font-bold pt-1 text-white', font.className)}>Genius</h1>
       </Link>
       <div className="flex itemsc  gap-x-2">
         <Link href={isSignedIn ? '/dashboard' : '/signup'}>
