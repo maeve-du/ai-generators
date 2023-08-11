271f7bd18e2c4263de7edca7ead04d004580ad79
Maeve Du <maeve.du@hotmail.com>
2023-08-11 15:37:42 +0800

    change empty photo

--- components/Empty.tsx
+++ components/Empty.tsx
@@ -4,12 +4,7 @@ const Empty = ({ label }: { label: string }) => {
   return (
     <div className="h-full p-6 md:p-20 flex flex-col items-center justify-center">
       <div className="relative h-72 w-72">
-        <Image
-          fill
-          src="/empty.png"
-          alt="Empty state photo"
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        />
+        <Image fill src="/empty.png" alt="Empty state photo" />
       </div>
       <div className="text-muted-foreground text-sm text-center">{label}</div>
     </div>
